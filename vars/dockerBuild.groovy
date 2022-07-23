ff35d110b04d6908eef041f7d5876c071bab1f25
bgradecoding <iinee12@kakao.com>
2022-07-23 21:01:40 +0900

    jenkinsfile change

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,11 +1,7 @@
 def call( String gitCommitTag ){
     println 'git commit Tag' + gitCommitTag
     
-    def stdout = new StringWriter()
-    def stderr = new StringWriter()
-    ['/bin/sh', '-c', "docker build -t "+gitCommitTag].execute().waitForProcessOutput(stdout, stderr)
-
-    println "OUTPUT: " + stdout.toString()
-    println "ERRORS: " + stderr.toString()
-
+    sh """
+    docker build -t ${gitCommitTag} .
+    """
 }
