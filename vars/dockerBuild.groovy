66a6b0c64104413f72c35a797bca7263e26c0903
bgradecoding <iinee12@kakao.com>
2022-07-23 12:19:42 +0900

    jenkinsfile change

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,14 @@
 def call( String gitCommitTag ){
-    
+    def resultado = new StringBuilder()
+    def error     = new StringBuilder()
     println 'git commit Tag' + gitCommitTag
-    "docker build -t gitCommitTag .".execute()
-    "docker tag gitCommitTag gitCommitTag:latest".execute()
+        
+    
+    def comando = "docker build -t gitCommitTag .".execute()
+    comando.consumeProcessOutput(resultado, error)
+    comando.waitForOrKill(1000) 
+    def comando2 = "docker tag gitCommitTag gitCommitTag:latest".execute()
+    comando2.consumeProcessOutput(resultado, error)
+    comando2.waitForOrKill(1000) 
+
 }
