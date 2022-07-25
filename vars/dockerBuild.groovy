593271f9acfff44a493b360b5d44c9b34a96e71b
bgradecoding <iinee12@kakao.com>
2022-07-25 15:34:43 +0900

    jenkinsfile gitprecommit sha add

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -5,8 +5,9 @@ def call( String gitCommitTag, String gitPreCommitTag, String systemName ){
     println 'git pre commit Tag:========== ' + gitPreCommitTag
     
     def dockerBuildTag = systemName.trim() + '-' + gitCommitTag.trim()
-
+    def dockerPreBuildTag = systemName.trim() + '-' + gitPreCommitTag.trim()
     sh """
+    docker tag "${dockerPreBuildTag}" "${dockerPreBuildTag:"backup"}"
     docker build -t "${dockerBuildTag}" .
     """
 }
