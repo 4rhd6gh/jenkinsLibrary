1b232569633f4cef70f40c14655336a9baaaa9cf
bgradecoding <iinee12@kakao.com>
2022-07-25 14:20:12 +0900

    jenkinsfile change

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,12 +1,13 @@
-def call( String gitCommitTag, String systemName ){
+def call( String gitCommitTag, String gitPreCommitTag, String systemName ){
 
     println 'System:========== ' + systemName
     println 'git commit Tag:========== ' + gitCommitTag
+    println 'git commit Tag:========== ' + gitPreCommitTag
     
     sh """
     
     docker images --filter=reference='${systemName}*:latest'
-    docker tag (docker images --filter=reference='${systemName}*:latest') ${systemName}:before
+    docker tag (docker images --filter=reference='${systemName}*:latest') (${systemName}:before)
 
     docker build -t "${systemName}${gitCommitTag}" .
     """
